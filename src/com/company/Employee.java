b050fe653952f9b1c806f0d15e4a17582097d104
89081314528 <89081314528@mail.ru>
2019-09-02 22:02:09 +0300

    02/09 создала класс сотрудник

---
+++ src/com/company/Employee.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Employee {
+    String surname;
+    String name;
+    String middleName;
+    int passportSeries;
+    int passportNamber;
+    int salary;
+    public Employee (String surname, String name, String middleName, int passportSeries, int passportNamber, int salary) {
+        this.surname = surname;
+        this.name = name;
+        this.middleName = middleName;
+        this.passportSeries = passportSeries;
+        this.passportNamber = passportNamber;
+        this.salary = salary;
+    }
+
+}
