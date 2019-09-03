16b5c9a2858c25f8361c8edec73660cdf6fdbeb8
89081314528 <89081314528@mail.ru>
2019-09-03 22:09:57 +0300

    03/09 заполнила csv данными

--- src/com/company/Employee.java
+++ src/com/company/Employee.java
@@ -5,14 +5,14 @@ public class Employee {
     String name;
     String middleName;
     int passportSeries;
-    int passportNamber;
+    int passportNumber;
     int salary;
-    public Employee (String surname, String name, String middleName, int passportSeries, int passportNamber, int salary) {
+    public Employee (String surname, String name, String middleName, int passportSeries, int passportNumber, int salary) {
         this.surname = surname;
         this.name = name;
         this.middleName = middleName;
         this.passportSeries = passportSeries;
-        this.passportNamber = passportNamber;
+        this.passportNumber = passportNumber;
         this.salary = salary;
     }
 
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,9 @@
 package com.company;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+
 /**
  * * паспортный стол - заполнить эксель паспортными данными сотрудников (фио, серия, номер, зарплата).
  *  * сначала сделать по аналогии с PrimeTime для одного человека, потом сделать массив
@@ -11,7 +15,31 @@ package com.company;
 
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws FileNotFoundException {
+	String[] surname = new String[] {"Иванов", "Сорокин", "Середа"};
+	String[] name = new String[] {"Павел", "Игорь", "Михаил"};
+	String[] middleName = new String[] {"Святославович", "Михайлович", "Владимирович"};
+	int[] salary = new int[] {250000, 300000, 36000};
+	int[] passportSeries = new int[] {2004, 2006, 1996};
+	int[] passportNumber = new int[] {165685, 854525, 347851};
+
+	PrintStream preanter = new PrintStream(new File("data.csv"));
+	preanter.println ("фамилия" + ";" + "имя" + ";" + "отчество" + ";" + "серия" + ";" + "номер" + ";" + "зарплата");
+		for (int i = 0; i < surname.length; i++) {
+			Employee human = new Employee(surname[i], name[i], middleName[i], passportSeries[i], passportNumber[i], salary[i]);
+			printFi(human, preanter);
+		}
+		for (int i = 0; i < surname.length; i++) {
+			Employee human = new Employee(surname[i], name[i], middleName[i], passportSeries[i], passportNumber[i], salary[i]);
+			printFioSalary(human, preanter);
+		}
     }
+
+    public static void printFi (Employee a, PrintStream preanter) {
+    	preanter.println(a.surname + ";" + a.name);
+	}
+	public static void printFioSalary (Employee a, PrintStream preanter) {
+		preanter.println(a.surname + ";" + a.name + ";" + a.surname + ";" + ";" + ";" + a.salary);
+	}
+
 }
